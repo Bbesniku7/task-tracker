0bc8867076d84f9a7ec960cd6aca332464429129
Bbesniku7 <Bbesniku7>
2022-05-15 21:14:55 +0200

    router added  & finished task tracker

--- src/App.vue
+++ src/App.vue
@@ -1,89 +1,37 @@
 <template>
   <div class="container">
     <Header
-      @btn-click="showForm()"
+      @toggle-add-task="toggleAddTask"
       title="Task Tracker"
       :showAddTask="showAddTask"
     />
-    <div v-show="showAddTask">
-      <AddTask @add-task="addTask" />
-    </div>
-    <Tasks
-      :tasks="tasks"
-      @delete-task="deleteTask"
-      @toggle-reminder="toggleReminder"
-    />
+    <router-view :showAddTask="showAddTask"></router-view>
+    <Footer />
   </div>
 </template>
 
-
 <script>
-// Import Components
-import Header from "./components/Header.vue";
-import Tasks from "./components/Tasks.vue";
-import AddTask from "./components/AddTask.vue";
-
+// Import components
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 export default {
   name: "App",
   components: {
     Header,
-    Tasks,
-    AddTask,
+    Footer,
   },
   data() {
     return {
-      tasks: [],
       showAddTask: false,
     };
   },
   methods: {
-    showForm() {
+    toggleAddTask() {
       this.showAddTask = !this.showAddTask;
     },
-    async addTask(newTask) {
-      const res = await fetch("api/tasks", {
-        method: "POST",
-        headers: {
-          "Content-type": "application/json",
-        },
-        body: JSON.stringify(newTask),
-      });
-      const data = await res.json();
-      this.tasks = [...this.tasks, data];
-    },
-    deleteTask(id) {
-      if (confirm("Are you sure you want to Delete?")) {
-        this.tasks = this.tasks.filter((task) => task.id !== id);
-      }
-    },
-    toggleReminder(id) {
-      if (confirm("You want to toggle ON / OFF reminder ?")) {
-        this.tasks = this.tasks.map((task) =>
-          task.id === id ? { ...task, reminder: !task.reminder } : task
-        );
-      }
-    },
-    async getTasks() {
-      const res = await fetch("api/tasks");
-      const data = await res.json();
-      return data;
-    },
-    async getTask(id) {
-      try {
-        const res = await fetch(`api/tasks${id}`);
-        const data = await res.json;
-        return data;
-      } catch (error) {
-        console.log(error);
-      }
-    },
-  },
-  async created() {
-    this.tasks = await this.getTasks();
   },
 };
 </script>
-
 
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap");
---
+++ src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer>
+    <a href="/about">About</a>
+    <p>Author: © Besnik Qunaku</p>
+  </footer>
+</template>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Dancing+Script:wght@500&display=swap");
+
+a {
+  color: #333;
+}
+p {
+  padding-top: 10px;
+  font-size: 16px;
+  font-family: "Dancing Script", cursive;
+}
+footer {
+  padding-top: 20px;
+  text-align: center;
+}
+</style>
